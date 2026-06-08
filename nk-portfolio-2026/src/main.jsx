d0fbba9443fe345f4cace903b0da1d19d1c162f5
nandakishore7768-arch <knandakishore7768@gmail.com>
2026-06-08 10:33:34 +0530

    removed background

--- nk-portfolio-2026/src/main.jsx
+++ nk-portfolio-2026/src/main.jsx
@@ -8,20 +8,11 @@ import Education from './education'
 import Scroll from './untitles'
 import Project from './project'
 import Contact from './contact'
-import video from './assets/background-gif.mp4';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header></Header>  
-    <video 
-                src={video} 
-                autoPlay 
-                loop 
-                muted 
-                playsInline 
-                className="absolute inset-0 w-full h-full object-cover -z-10 fixed"
-    /> 
-    
+    <h1>Portfolio website</h1>
     <Scroll><Introduction></Introduction></Scroll>
     <Scroll><Skills></Skills></Scroll>
     <Scroll><Education></Education></Scroll>
